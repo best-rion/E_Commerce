0005f073a44b1c51f23ea37aad57df8b88f1fee6
best-rion <husayn.buet@gmail.com>
2024-09-18 02:52:56 +0600

    ok

--- eCommerce/__init__.py
+++ eCommerce/__init__.py
@@ -10,9 +10,12 @@ db.init_app(app)
 
 from eCommerce.Users.routes import users
 from eCommerce.Admins.routes import admins
+from eCommerce.Carts.routes import carts
+
 
 app.register_blueprint(users)
 app.register_blueprint(admins)
+app.register_blueprint(carts)
 
 
 
